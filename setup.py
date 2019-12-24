a04bf4095f61e325c37667b3998c5b8567009f23
AmirReza <amir.r.amirfatahi@gmail.com>
2019-12-24 11:06:15 +0330

    Add install_requires packages to setup.py
    Also changes the version in setup.py from 0.1dev to 0.1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-install_requires = []
+install_requires = ['redis', 'gunicorn']
 
 setup(
     name='simple_bitly',
-    version='0.1dev',
+    version='0.1',
     description='Url shortener wsgi application',
     long_description=open('README.md').read(),
     install_requires=install_requires,
