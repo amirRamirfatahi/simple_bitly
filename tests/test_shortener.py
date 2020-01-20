bab8b034ff6f32654ade561da3e298e4820b3bf1
pylover <vahid.mardani@gmail.com>
2020-01-20 13:13:56 +0330

    Cleaning up

--- tests/test_shortener.py
+++ tests/test_shortener.py
@@ -1,20 +1,19 @@
-import functools
-
-from bddrest import Given as Given_, status, when, response
+from bddrest import Given, status, when, response
 
 from simplebitly.shortener import app
 
 
-Given = functools.partial(Given_, app)
-
-
 def test_shortener(urandommock, redismock):
     with Given(
+        app,
         'Shortening a URL',
         verb='POST',
         json=dict(url='http://example.com')
     ):
         assert status == 201
         assert response.text == 'rmXGPMVQnrKAB'
+
+        when('URL is not valid', json=dict(url='invalidurl'))
+        assert status == 400
 
 
