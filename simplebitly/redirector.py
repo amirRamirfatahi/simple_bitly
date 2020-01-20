d99118bc8f4071c12bea7c90d07d2ebe1fe2d06c
pylover <vahid.mardani@gmail.com>
2020-01-20 14:15:41 +0330

    Migrating to yhttp

--- simplebitly/redirector.py
+++ simplebitly/redirector.py
@@ -1,21 +1,16 @@
-"""
-redirector.py is one of the two entry points of our application.
-It takes a POST request with the previously provided shorturl from generator
-and redirects to the original url
-"""
+from yhttp import Application, statuses
 
 from . import db
 
 
-def app(environ, start_response):
-    path = environ.get('PATH_INFO', '/')
-    originalurl = db.redis.get(path[1:])
-    if originalurl:
-        start_response(
-            '302 Found',
-            [('Location', originalurl.decode())]
-        )
-        return [b'OK.']
-    else:
-        start_response('404 Not Found', [])
-        return [b'Not Found.']
+app = Application()
+
+
+@app.route('/(.*)')
+def get(req, key):
+    originalurl = db.redis.get(key)
+    if not originalurl:
+        raise statuses.notfound()
+
+    raise statuses.found(originalurl.decode())
+
